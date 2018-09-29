4e08006e5fea814412206dc8f35e9b63087b8a52
aetherwalker <aetherwalker@refugesystems.net>
2018-09-29 12:39:41 -0400

    Syncing alpha development

--- app/app.js
+++ app/app.js
@@ -7,24 +7,24 @@ Gaze.vue = new Vue({
 		"g$Version": Gaze.version,
 		"g$Start": Date.now()
 	},
-	methods: {
+	"methods": {
 		"g$reconfigure": Gaze.reconfigure
 	},
-	mounted: function() {
+	"mounted": function() {
 		var update = function() {
 			Gaze.vue.route = window.location.hash.substring(2);
 		};
 		window.addEventListener("hashchange", update);
 		window.addEventListener("popstate", update);
 	},
-	computed: {
-		ViewComponent () {
+	"computed": {
+		"ViewComponent": function() {
 			return {
 				"template": Vue.templified("dashboards/" + this.route + ".html") || Vue.templified(this.route) || Vue.templified("dashboards/notfound.html")
 			};
 		}
 	},
-	render (h) {
+	"render": function(h) {
 		return h(this.ViewComponent);
 	}
 });
